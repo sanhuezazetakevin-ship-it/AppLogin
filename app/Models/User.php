77b326dc9a2b49f2093a00142329e161827a2cd3
Omar Perez <omarperezcueva809@gmail.com>
2026-06-02 01:04:26 -0500

    Avance Request,Resource y Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'google_id', // ¡AÑADIDO! Permite guardar el ID de Google sin errores de seguridad
     ];
 
     /**
